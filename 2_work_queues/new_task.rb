ee33e89e707010eda4df3fdc87d00b3518862049
Rustam Gasanov <rustamagasanov@gmail.com>
2015-05-09 01:36:08 +0300

    [+] added durability/persistance/prefetch example

--- 2_work_queues/new_task.rb
+++ 2_work_queues/new_task.rb
@@ -1,12 +1,14 @@
+# Messages durability
 require 'bunny'
 
 conn = Bunny.new
 conn.start
 
 ch = conn.create_channel
-q  = ch.queue("hello")
+# 1) durable: persist the queue even if server would be restarted
+q  = ch.queue("hello", durable: true)
 msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
-p msg
 
+# 2) persistent: tells RabbitMQ to save the message to disk(in order not to lose it)
 q.publish(msg, persistent: true)
 puts " [x] Sent #{msg}"
--- 2_work_queues/worker.rb
+++ 2_work_queues/worker.rb
@@ -4,14 +4,24 @@ conn = Bunny.new
 conn.start
 
 ch = conn.create_channel
-q  = ch.queue("hello")
+q  = ch.queue("hello", durable: true)
+
+# This tells RabbitMQ not to give more than one message to a worker at a time.
+# Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one.
+# Instead, it will dispatch it to the next worker that is not still busy.
+ch.prefetch(1)
 
 puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
-q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-  puts " [x] Received #{body}"
-  # imitate some work
-  sleep body.count(".").to_i
-  puts " [x] Done"
 
-  ch.ack(delivery_info.delivery_tag)
+begin
+  q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
+    puts " [x] Received #{body}"
+    # imitate some work
+    sleep body.count(".").to_i
+    puts " [x] Done"
+
+    ch.ack(delivery_info.delivery_tag)
+  end
+rescue Interrupt
+  conn.close
 end
